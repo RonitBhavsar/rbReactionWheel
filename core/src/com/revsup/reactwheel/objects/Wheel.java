f746790fbd7453b9e485c1b4b025e4abd4d18b2e
RevsUp <revsup@lab-311-007.cs.csubak.edu>
2018-06-21 10:53:44 -0700

    final

--- core/src/com/revsup/reactwheel/objects/Wheel.java
+++ core/src/com/revsup/reactwheel/objects/Wheel.java
@@ -105,7 +105,7 @@ public class Wheel extends ShapeRenderer {
         explosionSound = Gdx.audio.newSound(Gdx.files.internal("explode.wav"));
         victorySound = Gdx.audio.newSound(Gdx.files.internal("victory.mp3"));
 
-        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("That's What I Like.mp3"));
         music.setVolume(0.5f);
         music.play();
         music.setLooping(true);
